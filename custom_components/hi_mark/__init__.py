9ac210de7732c42df34620f5970ae753b79f66c1
RichardMizak <richard.mizak@kosickaakademia.sk>
2022-05-30 12:21:25 +0200

    commit

--- custom_components/hi_mark/__init__.py
+++ custom_components/hi_mark/__init__.py
@@ -1,9 +1,20 @@
 from hassapi import Hass
+from homeassistant.components.button import ButtonEntity
+
 DOMAIN = "hi_mark_service"
 
 ATTR_NAME = "hi"
 DEFAULT_NAME = "hi_mark"
 
+
+class MyButton(ButtonEntity):
+    # Implement one of these methods.
+
+    def press(self) -> None:
+        """Handle the button press."""
+
+    async def async_press(self) -> None:
+        """Handle the button press."""
 
 def setup(hass, config):
     config = Hass(hassurl="http://IP_ADDRESS:8123/", token="YOUR_HASS_TOKEN")
