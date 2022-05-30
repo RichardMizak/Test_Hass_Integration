0c9512e81f45433c984ff281b4c66c06e8122797
RichardMizak <richard.mizak@kosickaakademia.sk>
2022-05-30 12:32:35 +0200

    commit

--- custom_components/hi_mark/__init__.py
+++ custom_components/hi_mark/__init__.py
@@ -7,14 +7,30 @@ ATTR_NAME = "hi"
 DEFAULT_NAME = "hi_mark"
 
 
-class MyButton(ButtonEntity):
-    # Implement one of these methods.
+from homeassistant.components.switch import SwitchEntity
 
-    def press(self) -> None:
-        """Handle the button press."""
 
-    async def async_press(self) -> None:
-        """Handle the button press."""
+class MySwitch(SwitchEntity):
+    def __init__(self):
+        self._is_on = False
+
+    @property
+    def name(self):
+        """Name of the entity."""
+        return "My Switch"
+
+    @property
+    def is_on(self):
+        """If the switch is currently on or off."""
+        return self._is_on
+
+    def turn_on(self, **kwargs):
+        """Turn the switch on."""
+        self._is_on = True
+
+    def turn_off(self, **kwargs):
+        """Turn the switch off."""
+        self._is_on = False
 
 def setup(hass, config):
     config = Hass(hassurl="http://IP_ADDRESS:8123/", token="YOUR_HASS_TOKEN")
